420fe4fd96546ce1eead84f5835ea7449753ca37
aluno <aluno@192.168.0.217>
2024-11-27 13:49:11 -0300

    Meu primeiro Commit - Meu Primeiro Programa em JAVA (Aula E.E Bernado Monteiro)

---
+++ primeiroprograma/src/primeiroprograma/Executavel.java
@@ -0,0 +1,11 @@
+package primeiroprograma;
+
+public class Executavel {
+
+	public static void main(String[] args) {
+		System.out.println("olá mundo");
+		System.out.println("Meu nome é Sophia");
+		
+	}
+
+}
