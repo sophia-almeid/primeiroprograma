b4f344f90031ac51446d10cd8220b78ff46b22f5
aluno <aluno@192.168.0.217>
2024-11-29 10:40:58 -0300

    Terceiro Commit

--- primeiroprograma/src/primeiroprograma/Executavel.java
+++ primeiroprograma/src/primeiroprograma/Executavel.java
@@ -6,17 +6,17 @@ public class Executavel {
 		System.out.println("olá mundo");
 		System.out.println("Meu nome é Sophia");
 		
-		int inteiro
-		double numeroDecimal
-		string texto
+		int inteiro;
+		double numeroDecimal;
+		String texto;
 		
 		inteiro = 5;
 		numeroDecimal = 5.65;
-		texto "Sophia";
+		texto = "Sophia";
 		
-		System.out.println("Nome:" + texto);
-		System.out.println("Numero inteiro:" + inteiro);
-		
+		System.out.println("Nome: " + texto);
+		System.out.println("Numero inteiro: " + inteiro);
+		System.out.println("Numero decimal: " + numeroDecimal);
 		
 		
 	}
